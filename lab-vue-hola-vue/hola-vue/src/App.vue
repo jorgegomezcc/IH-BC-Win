7adf69b2aaabed8f3f2f05cabb033bba5b00aaaa
Jorge Gomez <andispotlight@gmail.com>
2023-07-28 13:47:30 +0200

    adding lab vue

---
+++ lab-vue-hola-vue/hola-vue/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import NavApp from "./components/Nav.vue"
+import HeaderAppHome from "./components/Header.vue"
+import SectionAppHome from "./components/Section.vue"
+
+</script>
+
+
+<template>
+  <div id="app">
+    <NavApp/>
+    <HeaderAppHome/>
+    <SectionAppHome/>
+    
+  </div>
+</template>
+
+
+
+<style scoped>
+</style>
---
+++ lab-vue-hola-vue/hola-vue/src/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+
+
+<template>
+<div class="header-app-home">
+    <img src="../assets/images/Vue_logo.png" alt="">
+    <h1>Welcome to Vue.js</h1>
+    <h4>Become a coding ninja with the fastest-growing JavaScript framework</h4>
+    <button>Awesome!</button>
+</div>   
+</template>
+  
+ 
+  
+<style scoped>
+</style>
