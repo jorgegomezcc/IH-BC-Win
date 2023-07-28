8e084596adb9ba32ba64ef213033ade754ca669b
Jorge Gomez <andispotlight@gmail.com>
2023-07-28 21:51:18 +0200

    lab countries vue added

---
+++ lab-vue-wiki-countries/src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <!-- Navbar -->
+    <nav class="navbar navbar-dark bg-primary mb-3">
+      <div class="container">
+        <a class="navbar-brand" href="/">WikiCountries</a>
+      </div>
+    </nav>
+  </template>
+  
+  <script setup></script>
+  
+  <style scoped></style>
--- lab-vue-wiki-countries/src/router/index.js
+++ lab-vue-wiki-countries/src/router/index.js
@@ -1,23 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import CountryDetails from "../components/CountryDetails.vue";
+import CountriesList from "../components/CountriesList.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+
+  history: createWebHistory("/"),
+
+  scrollBehavior() {
+    document.getElementById("app").scrollIntoView();
+  },
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "list",
+      component: CountriesList,
+      children: [
+        {
+          path: "/list/:alpha3Code",
+          name: "list",
+          component: CountryDetails,
+        },
+      ],
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
